514e0838b7a1d40c77236b575faef5a240fa87c2
Luca Favaretto <lucafavaretto@icloud.com>
2023-10-03 15:40:47 +0200

    piccola modifica es 7

--- D2.js
+++ D2.js
@@ -85,9 +85,9 @@ console.log(`Questo è il tuo totale già scontato ${totalShoppingCartScontato}`
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let a = 200;
-let b = 55;
-let c = 100;
+let a = 300;
+let b = 1000;
+let c = 200;
 
 if (a > b && a > c) {
   if (b > c) {
@@ -109,6 +109,24 @@ if (c > a && c > b) {
   } else {
     console.log(`${c}>${b}>${a}`);
   }
+}
+
+if (a > b && a > c) {
+  if (b > c) {
+    console.log(`${a}>${b}>${c}`);
+  } else {
+    console.log(`${a}>${c}>${b}`);
+  }
+} else if (b > a && b > c) {
+  if (a > c) {
+    console.log(`${b}>${a}>${c}`);
+  } else {
+    console.log(`${b}>${c}>${a}`);
+  }
+} else if (b > a) {
+  console.log(`${c}>${b}>${a}`);
+} else {
+  console.log(`${c}>${a}>${b}`);
 }
 
 /* ESERCIZIO 8
